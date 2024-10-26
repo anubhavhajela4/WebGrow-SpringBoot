a4ddfe46de2e737a57e388ec396583f190837596
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-10-26 14:44:09 +0530

    Created User Registration mapping

---
+++ src/main/java/com/example/authentication/registration/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.example.authentication.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user/registration") //path used is different from the one used in video
+@AllArgsConstructor
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+}
