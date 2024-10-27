6b0c65fe04cc43b523df5e0600974e5045eb7f54
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-10-27 12:38:42 +0530

    User details gets stored in the database temporarily

--- src/main/java/com/example/authentication/registration/RegistrationController.java
+++ src/main/java/com/example/authentication/registration/RegistrationController.java
@@ -15,6 +15,7 @@ public class RegistrationController {
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
+
         return registrationService.register(request);
     }
 }
--- src/main/java/com/example/authentication/registration/RegistrationService.java
+++ src/main/java/com/example/authentication/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.example.authentication.registration;
 
+import com.example.authentication.appuser.AppUser;
+import com.example.authentication.appuser.AppUserRole;
+import com.example.authentication.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail) {
+            throw new IllegalStateException("Invalid email");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.PARTICIPANT
+                )
+        );
     }
 }
